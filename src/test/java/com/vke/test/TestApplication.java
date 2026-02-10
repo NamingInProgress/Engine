9fc2c72a5eca2112b717543bbab29ee60a8b5b10
v22 <v22julian@gmail.com>
2026-02-10 21:16:54 +0100

    master schema fix

--- src/test/java/com/vke/test/TestApplication.java
+++ src/test/java/com/vke/test/TestApplication.java
@@ -1,6 +1,7 @@
 package com.vke.test;
 
 import com.vke.Config;
+import com.vke.api.app.Version;
 import com.vke.api.parsing.config.ConfigDocument;
 import com.vke.api.parsing.config.ConfigParser;
 import com.vke.api.parsing.config.schema.ConfigSchema;
@@ -21,15 +22,13 @@ import java.io.IOException;
 public class TestApplication {
 
     public static void main(String[] args) throws InterruptedException, IOException, ConfigParser.ConfigParseException, SchemaMismatchException {
-        Identifier ident = Identifier.of("schema/testschema.json");
+        Identifier ident = Identifier.of("schema/layouts.schema.json");
         char[] source = Utils.readCharsFromInputStream(ident.asInputStream());
         ConfigParser parser = new JsonParser();
         parser.setSource(source);
         ConfigDocument d = parser.parse();
         ConfigSchema schema = ConfigSchema.readVke(d, ident.getPath());
         System.out.println(schema);
-
-        System.exit(0);
 
         ConfigurationOption<Boolean> renderdoc = new ConfigurationOption<>("renderdoc", ConfigurationOption.Initializer.BOOLEAN);
 
@@ -38,6 +37,7 @@ public class TestApplication {
         EngineCreateInfo createInfo = new EngineCreateInfo();
         createInfo.releaseMode = false;
         createInfo.windowCreateInfo = new WindowCreateInfo("My Window");
+        createInfo.vulkanCreateInfo.apiVersion = new Version(1, 0, 3);
 
         TestPipelines.init();
         VKEngine engine = new VKEngine(createInfo);
