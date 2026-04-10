2468f2e2ec31e505539d6c5a5aa533b3761da08d
julian hohenhausen <julian@v22.v22>
2026-04-10 21:12:34 +0200

    fixed fatal error

--- src/test/java/com/vke/test/rendering/Main.java
+++ src/test/java/com/vke/test/rendering/Main.java
@@ -11,7 +11,7 @@ import com.vke.core.services.Services;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         EngineCreateInfo createInfo = new EngineCreateInfo("CUBE", "vke");
-        createInfo.releaseMode = false;
+        createInfo.releaseMode = true;
         //createInfo.vsync = true;
         createInfo.windowCreateInfo = new WindowCreateInfo("Cube test");
 
