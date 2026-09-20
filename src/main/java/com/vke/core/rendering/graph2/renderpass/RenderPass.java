10d8c3a95fdc62c2f6948916eeb0bfc12bda1e63
v22lel <v22julian@gmail.com>
2026-09-20 16:04:33 +0200

    added if statement as 2nd check

--- src/main/java/com/vke/core/rendering/graph2/renderpass/RenderPass.java
+++ src/main/java/com/vke/core/rendering/graph2/renderpass/RenderPass.java
@@ -140,9 +140,11 @@ public abstract class RenderPass {
             OutputTexture output = outputs[i];
             OutputTextureDef texDef = def.outputs[i];
             if (texDef.type != TextureType.SCREEN) {
-                if (output != null && output.texture != null) {
-                    for (Texture outputTexture : output.texture) {
-                        graph.releaseTexture(outputTexture);
+                if (texDef.source == null) {
+                    if (output != null && output.texture != null) {
+                        for (Texture outputTexture : output.texture) {
+                            graph.releaseTexture(outputTexture);
+                        }
                     }
                 }
             }
